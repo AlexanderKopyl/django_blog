fed712ad52011bd924e583d02c155508b1c934b1
Alex <alex@localhost.localdomain>
2020-03-04 21:11:35 +0200

    Add form page .You can add  post in main page if you login into admin panel

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,7 @@ class PostView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -24,13 +25,19 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+
+        form = PostForm(request.POST, request.FILES)
+        print(request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            # post.photo = request.POST['photo']
+            # post.text = request.POST['text']
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+        elif form.errors:
+            print(form.errors)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
